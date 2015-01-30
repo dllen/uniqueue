8093cef46836666cf0ee027fbae45e0245de36f2
yijingping <jingping.yi@gmail.com>
2015-01-31 02:07:46 +0800

    add test_weixin_tpl_msg test

--- apps/consumer/index.js
+++ apps/consumer/index.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 var express = require('express');
 var router = express.Router();
-
+var request = require('request');
 router.post('/success', function(req, res) {
   console.log(req.body); 
   // 模拟处理时间为1s
@@ -26,6 +26,29 @@ router.post('/timeout', function(req, res) {
       res.json({'ret':0});
   }, 1000 * 999);
 
+});
+
+router.post('/test_weixin_tpl_msg', function(req, res) {
+  console.log(req.body); 
+  var tpl_data = JSON.parse(req.body['data']);
+  var options = {
+    uri: 'http://wxstage.sephora.cn/index.php?g=Interface&m=Message&a=tplSend&token=ychodn1421823030',
+    method: 'POST',
+    json: {
+      'open_id': req.body['open_id'],
+      'template_id': req.body['template_id'],
+      'url': req.body['url'],
+      'data': tpl_data
+    }
+  };
+  request(options, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      console.log(body) // Print the shortened url.
+      res.json({'ret':0});
+    } else {
+      res.json({'ret':1, 'message':JSON.stringify(body)});
+    }
+  });
 });
 
 module.exports = router;
